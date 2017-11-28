36a49fcdb997914e2eb5b93b95652e684f08b2c5
Bill Grace <bill@billgrace.com>
2017-11-28 13:56:51 -0800

    A second commit

--- IbDataSifterGui.py
+++ IbDataSifterGui.py
@@ -19,6 +19,7 @@ def PrepareGui():
 	SharedVars.GuiOutputPathLabel.place(anchor='nw', relx=0.01,rely=0.08)
 	# File system - logged data
 	SharedVars.GuiLogDateListBox.place(anchor='nw', relx=0.01,rely=0.12)
+	SharedVars.GuiLogDateListBox.configure()
 	for date in SharedVars.TradedDayDirectories:
 		SharedVars.GuiLogDateListBox.insert(tkinter.END, date)
 
